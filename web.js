3445ac62126ca7fcfe3ecc4c422501f9c0f2900d
Ubuntu <ubuntu@ip-172-31-39-85.us-west-2.compute.internal>
2013-07-05 17:03:06 +0000

    Fixed error with read file

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+var buf = null;
+
+fs.readFile('/index.html', function (err, data) {
+  if (err) throw err;
+  buf = new Buffer(data);
+});
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+	var txt = buf === null ? "Index is not read" : buf.toString('utf-8');
+    response.send(txt);
 });
 
 var port = process.env.PORT || 5000;
